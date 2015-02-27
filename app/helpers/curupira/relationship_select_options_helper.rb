e5c80e881908f7e5dca5832dbb7890a28bfb54d8
Paulo Moura <paulociecomp@gmail.com>
2015-02-27 17:13:25 -0300

    role views

--- app/helpers/curupira/relationship_select_options_helper.rb
+++ app/helpers/curupira/relationship_select_options_helper.rb
@@ -2,4 +2,8 @@ module Curupira::RelationshipSelectOptionsHelper
   def active_user_groups_select_options
     [["Selecione um grupo", nil]] + Group.active.map { |g| [g.name, g.id] }
   end
+
+  def active_features_select_options
+    [["Selecione uma funcionalidade", nil]] + Feature.all.map { |f| [f.description, f.id] }
+  end
 end
