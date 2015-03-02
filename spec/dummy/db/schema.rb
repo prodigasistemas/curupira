341c438fc7f9fc2fbfccb220345fa5965074379c
rcasseb <rcasseb@gmail.com>
2015-03-02 17:17:50 -0300

    add value default true for active field to features

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 20150227034046) do
     t.boolean "active", default: true
   end
 
+  create_table "user_groups", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                                          null: false
     t.string   "name",                                           null: false
