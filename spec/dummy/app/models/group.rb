931105ce346d52585e8f9bc633d3aa64dcae4b01
rcasseb <rcasseb@gmail.com>
2015-03-03 16:55:05 -0300

    Nasted form and relationship to RoleGroup

--- spec/dummy/app/models/group.rb
+++ spec/dummy/app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ActiveRecord::Base
+  validates_presence_of :name
+  has_many :group_users
   has_many :role_groups
   has_many :roles, through: :role_groups
-  has_many :group_users
   has_many :users, through: :group_users
   accepts_nested_attributes_for :role_groups, reject_if: :all_blank, allow_destroy: :true
-  validates_presence_of :name
   scope :active, -> { where active: true }
 end
