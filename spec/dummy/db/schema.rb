b66537a821438b1b18fc9ee9130a6d7f00d39b09
rcasseb <rcasseb@gmail.com>
2015-03-03 16:55:05 -0300

    Nasted form and relationship to RoleGroup

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150304122755) do
-
   create_table "authorizations", force: :cascade do |t|
     t.integer  "feature_id"
     t.integer  "role_id"
