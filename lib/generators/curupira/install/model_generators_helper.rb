fc0831f90c7267ecfb3fd307ebdcd6314902d0a9
Paulo Moura <paulociecomp@gmail.com>
2015-03-04 23:23:49 -0300

    fix tests

--- lib/generators/curupira/install/model_generators_helper.rb
+++ lib/generators/curupira/install/model_generators_helper.rb
@@ -172,15 +172,10 @@ module Curupira
         <<-CONTENT
           has_many :role_groups
           has_many :roles, through: :role_groups
-<<<<<<< HEAD
           has_many :group_users
           has_many :users, through: :group_users
-=======
->>>>>>> bugfix in view and tests
           accepts_nested_attributes_for :role_groups, reject_if: :all_blank, allow_destroy: :true
           validates_presence_of :name
-          has_many :group_roles
-          has_many :roles, through: :group_roles
           scope :active, -> { where active: true }
         CONTENT
       end
