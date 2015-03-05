9296098f63de4a0758f2e9d427d81b6dfbc3d057
Paulo Moura <paulociecomp@gmail.com>
2015-03-04 22:49:12 -0300

    bugfix in view and tests

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305021426) do
+ActiveRecord::Schema.define(version: 20150305011948) do
 
   create_table "authorizations", force: :cascade do |t|
     t.integer  "feature_id"
@@ -75,13 +75,6 @@ ActiveRecord::Schema.define(version: 20150305021426) do
   create_table "roles", force: :cascade do |t|
     t.string  "name",                  null: false
     t.boolean "active", default: true
-  end
-
-  create_table "user_groups", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
