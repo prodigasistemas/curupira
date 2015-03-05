58a8b5af49aea65d41ee5ca9135ebbad48433d0f
Paulo Moura <paulociecomp@gmail.com>
2015-03-04 23:23:49 -0300

    fix tests

--- lib/generators/curupira/install/model_generators_helper.rb
+++ lib/generators/curupira/install/model_generators_helper.rb
@@ -176,8 +176,6 @@ module Curupira
           has_many :users, through: :group_users
           accepts_nested_attributes_for :role_groups, reject_if: :all_blank, allow_destroy: :true
           validates_presence_of :name
-          has_many :group_roles
-          has_many :roles, through: :group_roles
           scope :active, -> { where active: true }
         CONTENT
       end
--- spec/dummy/db/migrate/20150305011945_create_roles.rb
+++
@@ -1,8 +0,0 @@
-class CreateRoles < ActiveRecord::Migration
-  def change
-    create_table :roles do |t|
-      t.string   :name,   null: false
-      t.boolean  :active, default: true
-    end
-  end
-end
