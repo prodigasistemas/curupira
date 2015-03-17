6537eca44a8ecd206b7cc077c4004e665007a27c
Paulo Moura <paulociecomp@gmail.com>
2015-03-17 15:14:02 -0300

    just list group ths belongs to user

--- app/helpers/curupira/relationship_select_options_helper.rb
+++ app/helpers/curupira/relationship_select_options_helper.rb
@@ -1,6 +1,6 @@
 module Curupira::RelationshipSelectOptionsHelper
-  def active_user_groups_select_options
-    [["Selecione um grupo", nil]] + Group.active.map { |g| [g.name, g.id] }
+  def active_user_groups_select_options(user)
+    [["Selecione um grupo", nil]] + Group.joins(:users).where(users: { id: user }, active: true).map { |g| [g.name, g.id] }
   end
 
   def active_features_select_options
