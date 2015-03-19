d4a9a10a3423c8278159d85f919ef84d271b4491
Paulo Moura <paulociecomp@gmail.com>
2015-03-19 10:10:54 -0300

    fix model feature generator

--- lib/generators/curupira/install/model_generators_helper.rb
+++ lib/generators/curupira/install/model_generators_helper.rb
@@ -206,11 +206,7 @@ module Curupira
 
       def feature_model_content
         <<-CONTENT
-          validates_presence_of :name
-          has_many :feature_services
-          has_many :services, through: :feature_services
-          has_many :feature_action_labels
-          has_many :action_labels, through: :feature_action_labels
+          has_many :action_labels
         CONTENT
       end
 
