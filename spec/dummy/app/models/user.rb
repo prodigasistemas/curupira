6626d554bf984eac2e85175c4b8c8cb3b4d88950
rcasseb <rcasseb@gmail.com>
2015-03-20 10:34:52 -0300

    add roles to user by group

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :groups, through: :group_users
   has_many :role_group_users, through: :group_users
   accepts_nested_attributes_for :group_users, reject_if: :all_blank, allow_destroy: :true
+  accepts_nested_attributes_for :role_group_users, reject_if: :all_blank, allow_destroy: :true
   
   scope :all_belonging_to, -> (user) { includes(group_users: :group).where(groups: { id: user.groups }) }
 end
