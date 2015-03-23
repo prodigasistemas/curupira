d448bb1d218c3d0782c4b094ed6879c1a3df4cb6
rcasseb <rcasseb@gmail.com>
2015-03-23 11:54:32 -0300

    fix bugs at views

--- lib/curupira/rails/routes.rb
+++ lib/curupira/rails/routes.rb
@@ -3,9 +3,9 @@ module ActionDispatch::Routing
     def curupira_routes
       scope module: 'curupira' do
         resources :users, except: :destroy do
-          get 'groups/:group_id/roles', action: :permission, controller: :users
+          get 'groups/:group_id/roles', to: 'users#permission',as: 'permission', action: :permission, controller: :users
         end
-
+        
         resources :groups, except: :destroy
         resources :roles, except: :destroy
         resource :session, only: [:new, :create] do
